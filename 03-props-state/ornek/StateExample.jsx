9a079312f94b9ed0d4acc8257584d4a3ea93b09f
sinemablak <87177874+sinemablak@users.noreply.github.com>
2026-02-07 15:28:55 +0300

    Create StateExample.jsx

---
+++ 03-props-state/ornek/StateExample.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>{count}</h2>
+
+      <button onClick={() => setCount(count + 1)}>
+        Arttır
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
