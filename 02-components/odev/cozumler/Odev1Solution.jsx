6ffd988d204abdc6565e53ed28cabae7925f0bb1
sinemablak <87177874+sinemablak@users.noreply.github.com>
2026-02-07 15:25:22 +0300

    Create Odev1Solution.jsx

---
+++ 02-components/odev/cozumler/Odev1Solution.jsx
@@ -0,0 +1,12 @@
+function Profile() {
+  return (
+    <div>
+      <h2>Ad: Sinem</h2>
+      <h2>Soyad: Ablak</h2>
+      <h2>Meslek: Yazılım Eğitmeni</h2>
+      <h3>Hobiler:Yoga yapmak</h3>
+    </div>
+  );
+}
+
+export default Profile;
