af638664a229e7d5e1c411000c4f01ee3578ff2b
sinemablak <87177874+sinemablak@users.noreply.github.com>
2026-02-07 15:39:08 +0300

    Create useEffectExample.jsx

---
+++ 04-hooks/ornekle/useEffectExample.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+function Timer() {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <h2>Süre: {time}</h2>;
+}
+
+export default Timer;
