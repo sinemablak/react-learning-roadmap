b5574e804b0059506b401d5c35690b4c36851f6d
sinemablak <87177874+sinemablak@users.noreply.github.com>
2026-02-07 15:26:00 +0300

    Create Odev3Solution.jsx

---
+++ 02-components/odev/cozumler/Odev3Solution.jsx
@@ -0,0 +1,11 @@
+function ProductCard() {
+  return (
+    <div style={{ border: "1px solid gray", padding: "10px" }}>
+      <h2>Laptop</h2>
+      <p>Fiyat: 25000 TL</p>
+      <p>Yüksek performanslı laptop</p>
+    </div>
+  );
+}
+
+export default ProductCard;
