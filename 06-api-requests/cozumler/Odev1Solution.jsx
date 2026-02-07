53bdc41490ebb9fc1332e139e30f097565ddd05e
sinemablak <87177874+sinemablak@users.noreply.github.com>
2026-02-07 15:52:10 +0300

    Create Odev1Solution.jsx

---
+++ 06-api-requests/cozumler/Odev1Solution.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function UserList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(setUsers);
+  }, []);
+
+  return (
+    <ul>
+      {users.map(user => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default UserList;
